7e77e244ed5cbfe0fc9b89bc58f4e5c0c0423c31
Mohan <777emohan@gmail.com>
2026-07-15 17:10:03 +0530

    Update training and dataset code

--- train.py
+++ train.py
@@ -1,16 +1,9 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import math
-import time
-import os
-
-
-
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-# print(f"Using device: {device}")
-# if device == "cuda":
-#     print(f"GPU: {torch.cuda.get_device_name(0)}")
+# import torch.nn as nn
+# import torch.nn.functional as F
+# import math
+# import time
+# import os
 
 from utils.tokenizer import CharacterTokenizer
 
@@ -18,14 +11,33 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 print("Using device:",device)
 tokenizer = CharacterTokenizer("data/shakespeare.txt")
+dataset = ShakespeareDataset(tokenizer)
+
+x,y = dataset.get_batch(
+    split = "train",
+    batch_size=4,
+    context_length=8,
+    device=device,
+)
 print()
-print("Vocabulary size:",tokenizer.vocab_size)
+
+print("Input Shape:",x.shape)
+print("Target Shape:",y.shape)
 print()
-print("First 20 characters:")
-print(tokenizer.chars[:20])
+
+print("Input")
+print(tokenizer.decode(x[0].tolist()))
 print()
-sample="Hello"
-encoded = tokenizer.encode(sample)
-print("Sample:",sample)
-print("Encoded:",encoded)
-print("Decoded:",tokenizer.decode(encoded))
+print("Target")
+print(tokenizer.decode(y[0].tolist()))
+
+# print("Vocabulary size:",tokenizer.vocab_size)
+# print()
+# print("First 20 characters:")
+# print(tokenizer.chars[:20])
+# print()
+# sample="Hello"
+# encoded = tokenizer.encode(sample)
+# print("Sample:",sample)
+# print("Encoded:",encoded)
+# print("Decoded:",tokenizer.decode(encoded))
--- utils/dataset.py
+++ utils/dataset.py
@@ -1,5 +1,48 @@
-train_data
+import torch
 
-val_data
 
-get_batch()
+class ShakespeareDataset:
+
+    def __init__(self, tokenizer, train_split=0.9):
+
+        self.data = torch.tensor(
+            tokenizer.encode(tokenizer.text),
+            dtype=torch.long
+        )
+
+        n = int(train_split * len(self.data))
+
+        self.train_data = self.data[:n]
+
+        self.val_data = self.data[n:]
+
+    def get_batch(
+        self,
+        split,
+        batch_size,
+        context_length,
+        device,
+    ):
+
+        data = (
+            self.train_data
+            if split == "train"
+            else self.val_data
+        )
+
+        ix = torch.randint(
+            len(data) - context_length,
+            (batch_size,)
+        )
+
+        x = torch.stack([
+            data[i:i + context_length]
+            for i in ix
+        ])
+
+        y = torch.stack([
+            data[i + 1:i + context_length + 1]
+            for i in ix
+        ])
+
+        return x.to(device), y.to(device)
